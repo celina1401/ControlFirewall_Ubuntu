463fb80ba9ca7d8f97efa49b339270534fbf2e98
celina1401 <nhkkhoacv@gmail.com>
2024-12-05 15:15:47 +0700

    donegit add .!

--- src/main/java/Frame/Logging.java
+++ src/main/java/Frame/Logging.java
@@ -281,7 +281,7 @@ public class Logging extends javax.swing.JFrame {
                     .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                         .addComponent(logLevel, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addComponent(logLevelEdit, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 31, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(logTablePanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(reloadLog))
@@ -343,13 +343,11 @@ public class Logging extends javax.swing.JFrame {
         }
         logStatus.setText(statusLogUpdate);
         logLevel.setText(levelLogUpdate);
-        
-
-        
+ 
     }//GEN-LAST:event_on_off_logActionPerformed
 
     private void logLevelEditActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_logLevelEditActionPerformed
-        String[] choices = {"low","medium","high"};
+        String[] choices = {"low","medium","high","full"};
         String level = (String) JOptionPane.showInputDialog(null, "Choose level", "Change log level",
                 JOptionPane.INFORMATION_MESSAGE,null,choices, choices[0]);
         logLevel.setText(logging_UFW.logLevel(host, port, username, password, level));    
